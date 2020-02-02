28c7fffa0311713200724fbd30d9ce7aec87a543
ymgyt <yamaguchi7073xtt@gmail.com>
2020-02-02 18:32:22 +0900

    add gracefull stop

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
-use log::{error, info};
+use env_logger::fmt::Color;
+use log::{error, info, Level, LevelFilter};
 use mysqlpinger::MySQLPinger;
-use std::time::Instant;
+use std::{io::Write, sync::Arc, time::Instant};
 
 fn main() {
     let start = Instant::now();
     let version = env!("CARGO_PKG_VERSION");
     let arg = mysqlpinger::arg_parser::new(version).get_matches();
 
-    init_signal_handler();
     init_logger(arg.is_present("silent"), arg.occurrences_of("verbose"));
 
-    let mut pinger = match MySQLPinger::from_arg(&arg) {
-        Ok(pinger) => pinger,
+    let pinger = match MySQLPinger::from_arg(&arg) {
+        Ok(pinger) => Arc::new(pinger),
         Err(err) => {
             eprintln!("{}", err);
             std::process::exit(1);
         }
     };
+
+    let pinger2 = Arc::clone(&pinger);
+    ctrlc::set_handler(move || {
+        // when user put Ctrl+C
+        // NG: ^CINFO ...
+        // OK: ^C
+        //     INFO ...
+        println!("");
+        info!("Handle Ctrl+C [SIGINT] stopping pinger...");
+        pinger2.stop();
+
+        // force quit
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        std::process::exit(2);
+    })
+    .expect("Error setting Ctrl-C handler");
 
     match pinger.ping() {
         Ok(_) => info!("OK (elapsed {:.3}sec)", start.elapsed().as_secs_f64()),
@@ -24,27 +40,12 @@ fn main() {
             error!("{} (elapsed {:.3}sec)", err, start.elapsed().as_secs_f64());
             std::process::exit(1);
         }
-    }
-}
-
-fn init_signal_handler() {
-    ctrlc::set_handler(move || {
-        // when user put Ctrl+C
-        // NG: ^CINFO ...
-        // OK: ^C
-        //     INFO ...
-        println!("");
-        info!("Handle Ctrl+C[SIGINT]");
-        std::process::exit(2);
-    }).expect("Error setting Ctrl-C handler");
+    };
 }
 
 fn init_logger(silent: bool, verbose: u64) {
     let mut builder = env_logger::Builder::new();
 
-    use env_logger::fmt::Color;
-    use log::{Level, LevelFilter};
-    use std::io::Write;
     builder.format(|buf, record| {
         let level_color = match record.level() {
             Level::Trace => Color::White,
--- src/pinger.rs
+++ src/pinger.rs
@@ -1,20 +1,30 @@
 use clap::ArgMatches;
-use log::info;
+use log::{debug, info};
 use mysql::{Conn, Opts, OptsBuilder};
 use parse_duration;
-use std::{borrow::Cow, thread, time::Duration};
+use std::{
+    borrow::Cow,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread,
+    time::Duration,
+};
 
 type BoxError = Box<dyn std::error::Error>;
 
 pub struct MySQLPinger {
-    opts: Opts,
+    opts: Arc<Opts>,
     interval: Duration,
     forever: bool,
     max_retry: u64,
+    canceled: AtomicBool,
 }
 
 impl MySQLPinger {
     pub fn from_arg(m: &ArgMatches) -> Result<Self, BoxError> {
+        let interval = parse_duration::parse(m.value_of("interval").unwrap())?;
         // we need OptsBuilder type first, then calling building methods
         let mut builder = OptsBuilder::default();
         builder
@@ -28,25 +38,35 @@ impl MySQLPinger {
             .user(m.value_of("user"))
             .pass(m.value_of("pass"))
             .prefer_socket(false)
-            .db_name(m.value_of("dbname"));
+            .db_name(m.value_of("dbname"))
+            .tcp_connect_timeout(Some(interval));
 
-        let interval = parse_duration::parse(m.value_of("interval").unwrap())?;
 
         Ok(Self {
-            opts: builder.into(),
+            opts: Arc::new(builder.into()),
             interval,
             forever: m.is_present("forever"),
             max_retry: m.value_of("max_retry").unwrap().parse()?,
+            canceled: AtomicBool::new(false),
         })
     }
 
-    pub fn ping(&mut self) -> Result<(), BoxError> {
+    pub fn stop(&self) {
+        debug!("stop called");
+        self.canceled.store(true, Ordering::SeqCst)
+    }
+
+    pub fn ping(&self) -> Result<(), BoxError> {
         info!(
             "ping -> addr:{host}:{port} user:{user} db:{db}",
             host = self.opts.get_ip_or_hostname().unwrap_or(""),
             port = self.opts.get_tcp_port(),
             user = self.opts.get_user().unwrap_or(""),
             db = self.opts.get_db_name().unwrap_or(""),
+        );
+        debug!("interval:{interval:.1}sec attempt:{attempt}",
+            interval = self.interval.as_secs_f64(),
+            attempt = self.max_attempt_symbol(),
         );
 
         let mut attempt = 1;
@@ -55,10 +75,14 @@ impl MySQLPinger {
             if !self.forever && attempt > max_attempt {
                 return Err("Max retry count exceeded".into());
             }
+            if self.canceled.load(Ordering::SeqCst) {
+                return Err("Canceled".into());
+            }
 
             use mysql::DriverError;
             use mysql::Error::*;
-            match Conn::new(self.opts.clone()) {
+            let opts = Arc::clone(&self.opts);
+            match Conn::new(Opts::clone(&opts)) {
                 Ok(mut conn) => {
                     if conn.ping() {
                         return Ok(());
@@ -69,6 +93,9 @@ impl MySQLPinger {
                         let (_, description, _) = err;
                         info!("{}/{} {}", attempt, self.max_attempt_symbol(), description);
                     }
+                }
+                Err(DriverError(DriverError::ConnectTimeout)) => {
+                    info!("{}/{} {}", attempt, self.max_attempt_symbol(), "Connection timeout");
                 }
                 Err(err) => return Err(Box::new(err)),
             }
